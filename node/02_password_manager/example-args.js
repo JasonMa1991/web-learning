76f44107b578fe4a9e9f0d04e9b464fe9f0634b7
Jason Ma <jasonma@servian.co.nz>
2016-09-02 16:29:33 +1200

    Node - yargs (command line inputs)

--- node/02_password_manager/example-args.js
+++ node/02_password_manager/example-args.js
@@ -1,4 +1,24 @@
-var argv = require('yargs').argv
+var argv = require('yargs')
+	.command(
+		"hello",
+		"Greets the user",
+		function(yargs) {
+			yargs.options({
+				  name: {
+					  demand: true
+					, alias: 'n'
+					, description: "Your first name goes here"
+				}
+				, lastname: {
+					  demand: true
+					, alias: 'l'
+					, description: "Your last name goes here"
+				}
+			}).help("help");
+		}
+	)
+	.help("help")
+	.argv
 console.log(argv)
 
 var command = argv._[0]
@@ -6,9 +26,7 @@ var name = argv.name
 var lastname = argv.lastname
 
 names = ""
-if (typeof name === "string") { 
-	names += " " + name
-}
+if (typeof name === "string") { names += " " + name }
 if (typeof lastname === "string") { names += " " + lastname }
 if (names === "") {names = " World"}
 
