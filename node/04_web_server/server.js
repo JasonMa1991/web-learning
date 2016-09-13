698a2818c456256bafb484da3c5f9315abcb3715
Jason Ma <jasonma@servian.co.nz>
2016-09-13 14:33:00 +1200

    Node - logging with middleware

--- node/04_web_server/server.js
+++ node/04_web_server/server.js
@@ -10,7 +10,22 @@ function confirmStart(){
 	console.log("Express server started on port " + PORT + "!")
 }
 
-app.get("/about", displayAbout)
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log("private route hit!")
+		next()
+	} ,
+	logger: function (req, res, next) {
+		var current_time = new Date()
+		console.log("Request at " + current_time + ": " + req.method + " " + req.originalUrl)
+		next()
+	}
+}
+
+app.use(middleware.logger)
+
+app.get("/about", middleware.requireAuthentication, displayAbout)
 
 app.use(express.static(__dirname + "/public"))
 
