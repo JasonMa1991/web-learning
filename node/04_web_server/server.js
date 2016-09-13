2b3a899021fcd9e3c10d3f2c902a601973977112
Jason Ma <jasonma@servian.co.nz>
2016-09-13 13:47:08 +1200

    Node - extracted function

--- node/04_web_server/server.js
+++ node/04_web_server/server.js
@@ -1,9 +1,11 @@
 var express = require("express")
 var app = express()
 
-app.get("/", function (req, res) {
+app.get("/", displayResult)
+
+function displayResult(req, res) {
 	res.send("Hello Express!")
-})
+}
 
 app.listen(3000)
-// app.get("/about")
+app.get("/about")
