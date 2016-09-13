d4e1906a86b15898e54d412f92508e8d66702c95
Jason Ma <jasonma@servian.co.nz>
2016-09-13 13:58:52 +1200

    Node - serving a static page (changed port to upper case to indicate var shouldn't be changed)

--- node/04_web_server/server.js
+++ node/04_web_server/server.js
@@ -1,17 +1,17 @@
 var express = require("express")
 var app = express()
-var port = 3000
+var PORT = 3000
 
 function displayAbout(req, res) {
 	res.send("About Us")
 }
 
 function confirmStart(){
-	console.log("Express server started on port " + port + "!")
+	console.log("Express server started on port " + PORT + "!")
 }
 
 app.get("/about", displayAbout)
 
 app.use(express.static(__dirname + "/public"))
 
-app.listen(port, confirmStart)
+app.listen(PORT, confirmStart)
